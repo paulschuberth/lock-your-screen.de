c8c19ceb52fd2aab1f16d99ccf0b9c0191292954
Paul Schuberth <github@pschuberth.de>
2023-01-15 18:15:33 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  lockScreenShortCut(forOs: OS|undefined): string {
+    switch (forOs) {
+      case 'Windows': return 'Windows Key + L';
+      case 'Android':
+      case 'iOS': return 'Lock button';
+      case 'Linux': return 'Ctrl + Alt + L';
+      case 'macOS': return 'Ctrl + Command + Q';
+      default: return 'Unknown';
+    }
+  }
+
+  detectOS(): OS|undefined {
+    const userAgent = navigator.userAgent;
+    if (userAgent.includes('Windows NT')) {
+      return 'Windows';
+    }
+
+    if (userAgent.includes('like Mac')) {
+      return 'iOS';
+    }
+
+    if (userAgent.includes('Mac')) {
+      return 'macOS';
+    }
+
+    if (userAgent.includes('Android')) {
+      return 'Android';
+    }
+
+    if (userAgent.includes('Linux')) {
+      return 'Linux';
+    }
+
+    return undefined;
+  }
   title = 'lock-your-screen';
 }
+
+export type OS = 'macOS' | 'Windows' | 'Android' | 'iOS' | 'Linux'
